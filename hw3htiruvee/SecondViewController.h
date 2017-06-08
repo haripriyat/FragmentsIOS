35f76d631dc3c9adedc6cd82ec45f2125439858b
Yazhini Konguvel <yazhinikonguvel@gmail.com>
2017-06-08 18:43:00 -0400

    Initial Commit

---
+++ hw3htiruvee/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  hw3htiruvee
+//
+//  Created by Yazhini Konguvel on 6/8/17.
+//  Copyright © 2017 CarnegieMellonUniversity. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ hw3htiruveeTests/hw3htiruveeTests.m
@@ -0,0 +1,39 @@
+//
+//  hw3htiruveeTests.m
+//  hw3htiruveeTests
+//
+//  Created by Yazhini Konguvel on 6/8/17.
+//  Copyright © 2017 CarnegieMellonUniversity. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface hw3htiruveeTests : XCTestCase
+
+@end
+
+@implementation hw3htiruveeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
